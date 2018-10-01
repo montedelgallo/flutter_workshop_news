ac49974095b39bf28410ebb563923109e349dac4
Giovanni Panasiti <giova.panasiti@gmail.com>
2018-10-01 23:30:48 +0200

    add tabs to homepage

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,8 +6,71 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  PageController _tabController;
+  String _title;
+  int _index;
+
+  void onTap(int tab) {
+    _tabController.jumpToPage(tab);
+  }
+
+  void onTabChanged(int tab) {
+    setState(() {
+      this._index = tab;
+      this._title = TabItems[tab].title;
+    });
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new PageController();
+    _title = TabItems[0].title;
+    _index = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+        backgroundColor: Colors.black,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _index,
+          onTap: onTap,
+          items: TabItems.map((TabItem item) {
+            return BottomNavigationBarItem(
+              title: Text(item.title),
+              icon: Icon(item.icon, color: Colors.black,),
+            );
+          }).toList()
+      ),
+      body: PageView(
+        controller: _tabController,
+        onPageChanged: onTabChanged,
+        children: <Widget>[
+          Text('uno'),
+          Text('due'),
+          Text('tre')
+        ],
+      ),
+    );
   }
 }
+
+
+class TabItem {
+  final String title;
+  final IconData icon;
+
+  const TabItem({ this.title, this.icon });
+}
+
+const List<TabItem> TabItems = <TabItem>[
+  TabItem(title: 'Business', icon: Icons.monetization_on ),
+  TabItem(title: 'Science', icon: Icons.assignment ),
+  TabItem(title: 'Technology', icon: Icons.computer ),
+];
